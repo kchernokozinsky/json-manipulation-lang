a541a517b8817eb16c1596659b351beba2c1aa34
kchernokozinsky <kyrylo.chornokozynskyi@ukma.edu.ua>
2024-08-18 02:57:40 +0300

    main minor refactoring

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -2,7 +2,16 @@ use lexer::Lexer;
 use parser::jml;
 
 fn main() {
-    let source = r#" a = true b = 3 c = b d = a ---  {}"#;
+    let source = r#"
+a = 1
+d = "some_string"
+---
+
+{
+    "key1": a, 
+    "key2": d,
+    "key3": 1 
+}"#;
     let lexer = Lexer::new(source);
 
     let ast = jml::JmlParser::new().parse(source, lexer);
